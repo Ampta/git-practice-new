8482b18c91bcc098f76fb015b75c63d7cb2347fd
Shivam Gupta <ampta77000@gmail.com>
2026-09-06 14:42:33 +0530

    initial commit

---
+++ src/test/java/com/ampta/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ampta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
